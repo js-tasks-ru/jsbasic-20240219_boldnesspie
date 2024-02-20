c508e83be9e6ccae7e0d88afbc5c45eb428213be
Alex Haun <boldnesspie@gmail.com>
2024-02-20 18:53:00 +0300

    решение задач 2, 3, 4, 5 из 1 модуля

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,7 @@
 function truncate(str, maxlength) {
-  // ваш код...
+
+  // Чтобы не хардкодить в самом slice, заранее выставляем правильную длинну строки
+  maxlength -= 1;
+
+  return str.length > maxlength ? str.slice(0, maxlength) + '…' : str;
 }
