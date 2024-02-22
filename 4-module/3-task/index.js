d13d23a58d45ab3ed5e680274ec8b082631f0019
Alex Haun <boldnesspie@gmail.com>
2024-02-22 19:50:31 +0300

    решение 1, 2, 3 задач из модуля 4

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,39 @@
 function highlight(table) {
-  // ваш код...
+  const rows = table.querySelectorAll('tbody tr');
+  
+  for(let row of rows) {
+    let cells = row.cells;
+
+    for(let i = 1; i < cells.length; i++) {
+      let currentTd = cells[i];
+      let currentRow = currentTd.parentElement;
+
+      if( i === 1) {
+        if (Number(currentTd.textContent) < 18) {
+          currentRow.style.textDecoration = 'line-through';
+        }
+      }
+
+      if (i === 2) {
+        if (currentTd.textContent === 'm') {
+          currentRow.classList.add('male')
+          
+        } else {
+          currentRow.classList.add('female');
+        } 
+      }
+
+      if(i === 3) {
+        if (currentTd.dataset.available === 'true') {
+          currentRow.classList.add('available') 
+
+        } else if (currentTd.dataset.available === 'false') {
+            currentRow.classList.add('unavailable')
+
+        } else {
+          currentRow.hidden = true;
+        }
+      }
+    }
+  }
 }
