92fd14e895d4981b3a786da137d351b12d8b4b8c
Alex Haun <boldnesspie@gmail.com>
2024-02-27 20:17:30 +0300

    решение 1, 2 задач из модуля 5

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,8 @@
 function toggleText() {
-  // ваш код...
+  let btn = document.querySelector('.toggle-text-button');
+  let txt = document.querySelector('#text');
+  
+  btn.onclick = function() {
+    txt.hidden = !txt.hidden;
+  }
 }
