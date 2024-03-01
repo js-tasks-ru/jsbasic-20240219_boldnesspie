1d9a4e501e28a54c9a58faeddc12ea231450ba65
Alex Haun <boldnesspie@gmail.com>
2024-03-01 14:58:48 +0300

    Решение 3 задачи из модуля 5

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,36 @@
 function initCarousel() {
-  // ваш код...
+
+  let leftBtn = document.querySelector('.carousel__arrow_left');
+  let rightBtn = document.querySelector('.carousel__arrow_right');
+
+  let slides = document.querySelector('.carousel__inner');
+  let slideWidth = slides.offsetWidth;
+  let slidesLength = slides.children.length - 1; // 1 слайд показывается сразу
+
+  let counter = 0;
+  leftBtn.style.display = 'none';
+
+  leftBtn.addEventListener('click', previous);
+  rightBtn.addEventListener('click', next);
+
+  function previous() {
+    rightBtn.style.display = '';
+    counter--;
+    slides.style.transform = `translateX(-${slideWidth * counter}px)`;
+    
+    if(counter === 0) {
+      leftBtn.style.display = 'none';
+    }
+  }
+
+  function next() {
+    leftBtn.style.display = '';
+    counter++;
+    slides.style.transform = `translateX(-${slideWidth * counter}px)`;
+
+    if (counter === slidesLength) {
+      rightBtn.style.display = 'none';
+      counter = slidesLength;
+    }
+  }
 }
